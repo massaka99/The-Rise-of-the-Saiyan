11d7105f578716f443941cad1d1bfbdbabbd36ce
bassamsalou <125871031+bassamsalou@users.noreply.github.com>
2024-09-18 20:16:31 +0200

    git conflict fixed
    tilbage til seneste version uden bassams animations

--- TheRiseOfTheSaiyan/Assets/_GAME_/Player/Scripts/Player_Controller.cs
+++ TheRiseOfTheSaiyan/Assets/_GAME_/Player/Scripts/Player_Controller.cs
@@ -5,164 +5,104 @@ using UnityEngine;
 [SelectionBase]
 public class Player_Controller : MonoBehaviour
 {
-    public float moveSpeed;
+    #region Enums
+    private enum Directions{UP, DOWN, LEFT, RIGHT}
+    #endregion
 
-    private bool isMoving;
+    #region Editor Data
 
-    private Vector2 input;
+    [Header("Movement Attributes")]
+    [SerializeField] float _moveSpeed = 50f;
 
-    private Animator animator;
+    [Header("Dependencies")]
+    [SerializeField] Rigidbody2D _rb;
+    [SerializeField] Animator _animator;
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    #endregion
 
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-    }
+    #region Internal Data
+    private Vector2 _moveDir = Vector2.zero;
+    private Directions _facingDirections = Directions.RIGHT;
+    #endregion
+
+    #region Tick
 
     private void Update()
     {
-        if (!isMoving)
-        {
-            input.x = Input.GetAxisRaw("Horizontal");
-            input.y = Input.GetAxisRaw("Vertical");
-
-            animator.SetFloat("moveX", input.x);
-            animator.SetFloat("moveY", input.y);
-
-            if (input.x != 0) input.y = 0;
-
-            if (input != Vector2.zero)
-            {
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-
-                StartCoroutine(Move(targetPos));
-            }
-
-        }
+        GatherInput();
+        CalculateFacingDirection();
+        UpdateAnimations();
     }
 
-    IEnumerator Move(Vector3 targetPos)
+    private void FixedUpdate()
     {
-        isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPos;
-
-        isMoving = false;
+        MovementUpdate();
     }
 
+    #endregion
 
+    #region Input Logic
 
+    private void GatherInput()
+    {
+        _moveDir.x = Input.GetAxisRaw("Horizontal");
+        _moveDir.y = Input.GetAxisRaw("Vertical");
+    }
 
+    #region Movement Logic
 
+    private void MovementUpdate()
+    {
+        _rb.velocity = _moveDir.normalized * _moveSpeed * Time.fixedDeltaTime;
+    }
 
+    #endregion
 
+    #region Animations Logic
 
+    private void CalculateFacingDirection()
+    {
+        if (_moveDir.x != 0)
+        {
+            _animator.SetBool("IsMovingSide", true);
+            if (_moveDir.x > 0) // move Right
+            {
+                _facingDirections = Directions.RIGHT;
+            }
+            else if (_moveDir.x < 0) // move Left
+            {
+                _facingDirections = Directions.LEFT;
+            }
+        }
 
+        if (_moveDir.y != 0)
+        {
+            _animator.SetBool("IsMovingUp", true);
+            if (_moveDir.y > 0) // move UP
+            {
+                
+                _facingDirections = Directions.UP;
+            }
+            else if (_moveDir.y < 0) // move DOWN
+            {
+                _facingDirections = Directions.DOWN;
+            }
+        }
+    }
 
+    private void UpdateAnimations()
+    {
+        if (_facingDirections == Directions.LEFT)
+        {
+            _spriteRenderer.flipX = true; 
+        }
+        else if (_facingDirections == Directions.RIGHT)
+        {
+            _spriteRenderer.flipX = false;
+        }
+    }
 
-    //#region Enums
-    //private enum Directions{UP, DOWN, LEFT, RIGHT}
-    //#endregion
+    #endregion
 
-    //#region Editor Data
-
-    //[Header("Movement Attributes")]
-    //[SerializeField] float _moveSpeed = 50f;
-
-    //[Header("Dependencies")]
-    //[SerializeField] Rigidbody2D _rb;
-    //[SerializeField] Animator _animator;
-    //[SerializeField] SpriteRenderer _spriteRenderer;
-    //#endregion
-
-    //#region Internal Data
-    //private Vector2 _moveDir = Vector2.zero;
-    //private Directions _facingDirections = Directions.RIGHT;
-    //#endregion
-
-    //#region Tick
-
-    //private void Update()
-    //{
-    //    GatherInput();
-    //    CalculateFacingDirection();
-    //    UpdateAnimations();
-    //}
-
-    //private void FixedUpdate()
-    //{
-    //    MovementUpdate();
-    //}
-
-    //#endregion
-
-    //#region Input Logic
-
-    //private void GatherInput()
-    //{
-    //    _moveDir.x = Input.GetAxisRaw("Horizontal");
-    //    _moveDir.y = Input.GetAxisRaw("Vertical");
-    //}
-
-    //#region Movement Logic
-
-    //private void MovementUpdate()
-    //{
-    //    _rb.velocity = _moveDir.normalized * _moveSpeed * Time.fixedDeltaTime;
-    //}
-
-    //#endregion
-
-    //#region Animations Logic
-
-    //private void CalculateFacingDirection()
-    //{
-    //    if (_moveDir.x != 0)
-    //    {
-    //        _animator.SetBool("IsMovingSide", true);
-    //        if (_moveDir.x > 0) // move Right
-    //        {
-    //            _facingDirections = Directions.RIGHT;
-    //        }
-    //        else if (_moveDir.x < 0) // move Left
-    //        {
-    //            _facingDirections = Directions.LEFT;
-    //        }
-    //    }
-
-    //    if (_moveDir.y != 0)
-    //    {
-    //        _animator.SetBool("IsMovingUp", true);
-    //        if (_moveDir.y > 0) // move UP
-    //        {
-
-    //            _facingDirections = Directions.UP;
-    //        }
-    //        else if (_moveDir.y < 0) // move DOWN
-    //        {
-    //            _facingDirections = Directions.DOWN;
-    //        }
-    //    }
-    //}
-
-    //private void UpdateAnimations()
-    //{
-    //    if (_facingDirections == Directions.LEFT)
-    //    {
-    //        _spriteRenderer.flipX = true; 
-    //    }
-    //    else if (_facingDirections == Directions.RIGHT)
-    //    {
-    //        _spriteRenderer.flipX = false;
-    //    }
-    //}
-
-    //#endregion
-
-    //#endregion
+    #endregion
 }
